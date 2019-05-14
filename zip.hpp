c9aadc217007c8f4e2704ddab30573637a30dd8e
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-14 17:58:53 +0300

    Update zip.hpp

--- zip.hpp
+++ zip.hpp
@@ -1,88 +1,47 @@
 #pragma once
-#include <iostream>
+
+using namespace std;
 
 namespace itertools{
-    template<typename T, typename U> 
-    class zip{
-         private:
-            T _item1;
-            U _item2;
+	
+	template<typename T,typename S> class zip{
 
-        public:
+		public:
 
-            zip(const T item1 , const U item2): _item1(item1), _item2(item2) {}
-            class iterator{
+            T start;
+            S start2; 
 
+            zip(T firstValue, S secondValue) : start(firstValue), start2(secondValue){}
+            
+            template<typename A,typename B> class iterator {
                 private:
-                    typename T::iterator _it1;
-                    typename T::iterator _it1End;
-                    typename U::iterator _it2;
+
+                A value;
+                B value2;
 
                 public:
-                    iterator(typename T::iterator item1,typename T::iterator item1End , typename U::iterator item2)
-                    : _it1(item1), _it1End(item1End), _it2(item2) {}
+                iterator(A firstStart, B secondStart):  value(firstStart),value2(secondStart){}
 
-		            auto operator*()  {
-                        return pair(*(_it1),*(_it2));
-		            }
+                T operator*() const {
+                    return value;
+                }
 
-		            auto operator->() const {
-                       return pair(&(_it1),&(_it2));
-		            }
-                    
-		            iterator& operator++() {/* Prefix operator ++ implementation.(++i)  */
-                        _it1++;
-                        _it2++;
-			            return *this;
-		            }
-
-		            const iterator operator++(int) {/* Postfix operator ++ implementation.(i++)  */
-                        iterator it = *this; 
-                         ++*this; 
-                        return it; 
-		            }
-
-                    bool operator==(const iterator& rhs) const {/* Operator == implementation. Return true iff _it == rhs._it */
-			            return (_it1 == rhs._it1) && (_it2 == rhs._it2);
-		            }
-
-		            bool operator!=(const iterator& rhs) const {/* Operator != implementation. Return true iff _it != rhs._it */
-			            return (_it1 != rhs._it1) && (_it2 != rhs._it2);
-                    }
-
+                iterator* operator++() {
+                    value++;
+                    return this;
+                }
+                
+                bool operator!=(const iterator& other) const {
+                    return value != (other.value);
+                }
             };
 
-            typename zip::iterator begin() {
-		        return zip::iterator(_item1.begin(),_item1.end() ,_item2.begin());
-	        }
-
-	        typename zip::iterator end() {
-		        return zip::iterator(_item1.end(), _item1.end(), _item2.end());
+            auto begin() const{
+                return (start.begin(), start2.begin());
             }
-
-    };
-
-    template <typename T, typename U> class pair {
-        private:
-
-            T _first;
-            U _second;
-
-        public:
-
-            pair(T first, U second): _first(first), _second(second) {}
-
-            template <typename T1, typename T2>
-            friend std::ostream& operator <<(std::ostream& os, const pair<T1,T2> thepair);
-
             
-    };
-}
-
-
-template <typename T1, typename T2> 
-            ostream& operator<< (ostream& out, 
-            const pair<T1,T2>& thepair) {
-                out << thepair.first << "," << thepair.second;
-                return out;
+            auto end() const{
+                return (start.end(), start2.end());
             }
+	};
+};
