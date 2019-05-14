9231df06a0d596ed6902bce60cf5b39a039e2c23
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-14 17:59:31 +0300

    Update chain.hpp

--- chain.hpp
+++ chain.hpp
@@ -1,78 +1,49 @@
 #pragma once
-#include <iostream>
-#include <string>
 
 using namespace std;
+
 namespace itertools{
-    template<typename T, typename U> class chain{
+	
+	template<typename T,typename S> class chain{
 
-        private:
-            T _item1;
-            U _item2;
+		public:
 
-        public:
+            T item1;
+            S item2; 
 
-            chain(const T item1 , const U item2): _item1(item1), _item2(item2) {}
-            class iterator{
+            chain(T firstValue, S secondValue) : item1(firstValue), item2(secondValue){}
+            
+            template<typename A,typename B> class iterator {
                 
                 private:
 
-                    typename T::iterator _it1;
-                    typename T::iterator _it1End;
-                    typename U::iterator _it2;
+                    A value;
+                    B value2;
 
                 public:
 
-                    iterator(typename T::iterator item1, typename T::iterator item1End, typename U::iterator item2)
-                    : _it1(item1), _it1End(item1End), _it2(item2) {}
+                    iterator(A firstStart, B secondStart):  value(firstStart), value2(secondStart){}
 
-		            auto operator*()  {
-                        if(_it1 != _it1End){
-                            return *(_it1);
-                        }
-			            return *(_it2);
-		            }
+                    T operator*() const {
+                        return value;
+                    }
 
-		            auto operator->() const {
-                        if(_it1 != _it1End){
-                            return &(_it1);
-                        }
-			            return &(_it2);
-		            }
+                    iterator* operator++() {
+                        value++;
+                        return this;
+                    }
 
-		            /* Prefix operator ++ implementation.(++i)  */
-		            iterator& operator++() {
-                        if(_it1 != _it1End){
-                            (_it1)++;
-			                return *this;
-                        }
-                        (_it2)++;
-			            return *this;
-		            }
-
-		            /* Postfix operator ++ implementation.(i++)  */ 
-		            const iterator operator++(int) {
-                        iterator it = *this; 
-                         ++*this; 
-                        return it; 
-		            }
-                    
-                    bool operator==(const iterator& rhs) const {
-			            return (_it1 == rhs._it1) && (_it2 == rhs._it2);
-		            }
-
-		            bool operator!=(const iterator& rhs) const {
-			            return (_it1 != rhs._it1) && (_it2 != rhs._it2);
+                    bool operator!=(const iterator& other) const {
+                        return value != (other.value);
                     }
             };
 
-            typename chain::iterator begin() {
-		        return chain::iterator(_item1.begin(),_item1.end() ,_item2.begin());
-	        }
-
-	        typename chain::iterator end() {
-		        return chain::iterator(_item1.end(), _item1.end(), _item2.end());
+            auto begin() const{
+                return (item1.begin(), item2.begin());
             }
-
-    };
-}
+            
+            auto end() const{
+                return (item1.end(), item2.end());
+            }
+	};
+};
