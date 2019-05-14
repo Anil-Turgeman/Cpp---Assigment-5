530879dacf016951c77c96c53de789a3da2a6177
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-14 17:59:46 +0300

    Update product.hpp

--- product.hpp
+++ product.hpp
@@ -1,71 +1,48 @@
 #pragma once
-#include <iostream>
-#include "zip.hpp"
+
+using namespace std;
 
 namespace itertools{
-    template<typename T, typename U> class product{
-         private:
+	
+	template<typename T,typename S> class product{
 
-            T _item1;
-            U _item2;
+		public:
 
-        public:
+            T start;
+            S start2; 
 
-            product(const T item1 , const U item2): _item1(item1), _item2(item2) {}
-
-            class iterator{
-
+            product(T firstValue, S secondValue) : start(firstValue), start2(secondValue){}
+            
+            template<typename A,typename B> class iterator {
                 private:
-                    typename T::iterator _it1;
-                    typename T::iterator _it1End;
-                    typename U::iterator _it2;
-                    typename U::iterator _it2End;
+
+                    A value;
+                    B value2;
 
                 public:
-                    iterator(typename T::iterator item1,typename T::iterator item1End, 
-                    typename U::iterator item2 ,typename U::iterator item2End )
-                    : _it1(item1), _it1End(item1End), _it2(item2), _it2End(item2End) {}
 
-		            auto operator*()  {
-                        return std::pair(*(_it1),*(_it2));
-		            }
+                    iterator(A firstStart, B secondStart):  value(firstStart),value2(secondStart){}
 
-		            auto operator->() const {
-                       return std::pair(&(_it1),&(_it2));
-		            }
-                    
-                    
-		            iterator& operator++() {/* Prefix operator ++ implementation.(++i)  */
-                        if(_it1 != _it1End && _it2 != _it2End){
-                            _it1++;
-                            _it2++;
-                        }
-                       
-			            return *this;
-		            }
-
-                     
-		            const iterator operator++(int) {/* Postfix operator ++ implementation.(i++)  */
-                        iterator it = *this; 
-                         ++*this; 
-                        return it; 
-		            }
-
-                    bool operator==(const iterator& rhs) const {
-			            return (_it1 == rhs._it1) && (_it2 == rhs._it2);
-		            }
-
-		            bool operator!=(const iterator& rhs) const {
-			            return (_it1 != rhs._it1) && (_it2 != rhs._it2);
+                    T operator*() const {
+                        return value;
                     }
-            };
-       
-            typename product::iterator begin() {
-		        return product::iterator(_item1.begin(),_item1.end() ,_item2.begin(),_item2.end());
-	        }
+ 
+                    iterator* operator++() {
+                        value++;
+                        return this;
+                    }
 
-	        typename product::iterator end() {
-		        return product::iterator(_item1.end(), _item1.end(), _item2.end(),_item2.end());
+                    bool operator!=(const iterator& other) const {
+                        return value != (other.value);
+                    }
+                };
+
+            auto begin() const{
+                return (start.begin(), start2.begin());
             }
-    };
-}
+                
+            auto end() const{
+                return (start.end(), start2.end());
+            }
+	};
+};
