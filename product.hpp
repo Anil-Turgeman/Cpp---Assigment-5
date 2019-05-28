4f0c6f16fd2f264dcabe82454038aef5cd350655
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-28 12:59:57 +0300

    full implementation

--- product.hpp
+++ product.hpp
@@ -4,45 +4,53 @@ using namespace std;
 
 namespace itertools{
 	
-	template<typename T,typename S> class product{
+	template<typename T, typename S> class product{
 
-		public:
+		private:
 
             T start;
             S start2; 
-
-            product(T firstValue, S secondValue) : start(firstValue), start2(secondValue){}
             
-            template<typename A,typename B> class iterator {
+            template<typename A, typename B> class iterator {
+
                 private:
 
                     A value;
                     B value2;
 
                 public:
+                    B temp = value2;
 
-                    iterator(A firstStart, B secondStart):  value(firstStart),value2(secondStart){}
+                    iterator(A firstStart, B secondStart):  value(firstStart), value2(secondStart){}
 
-                    T operator*() const {
-                        return value;
-                    }
+                    std::pair<decltype(*value), decltype(*value2)> operator*() const {
+				        return std::pair<decltype(*value), decltype(*value2)> (*value, *value2);
+			        }
  
-                    iterator* operator++() {
-                        value++;
-                        return this;
-                    }
+                    product::iterator<A,B> &operator++() {
+				        ++value2;;
+				        return *this;
+			        }
 
                     bool operator!=(const iterator& other) const {
-                        return value != (other.value);
+                        if (value2 == other.value2){
+                            value2 = temp;
+                            value++;
+                        }
+                        return value != other.value && value2 != other.value2;
                     }
                 };
 
+        public:
+
+            product(T firstValue, S secondValue) : start(firstValue), start2(secondValue){}
+
             auto begin() const{
-                return (start.begin(), start2.begin());
+                return product::iterator<decltype(start.begin()), decltype(start2.begin())> (start.begin(), start2.begin());
             }
                 
             auto end() const{
-                return (start.end(), start2.end());
+                return product::iterator<decltype(start.end()), decltype(start2.end())> (start.end(), iterastart2bel2.end());
             }
 	};
 };
