3177243b86184a64ca1da44438fb6eac4cb54c5b
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-28 13:25:12 +0300

    Update range.hpp

--- range.hpp
+++ range.hpp
@@ -1,60 +1,66 @@
-#pragma once
+namespace itertools
+{
+template <typename T>
+class range
+{
+private:
+  T _start;
+  T _stop;
 
-using namespace std;
+public:
+  range(T start, T stop)
+  {
+    _start = start;
+    _stop = stop;
+  }
+  int length() const{
+   return  (_stop-_start);
+  }
+  class iterator
+  {
+  private:
+    T _it;
+    T it_current;
 
-namespace itertools{
-	
-	template<typename T> class range{
+  public:
+    iterator(T pointer)
+    {
+       it_current = pointer;
+      _it = pointer ;    
+    }
+    T operator*() const
+    {
+      return it_current;
+    }
 
-		private:
+    // ++i;
+    iterator operator++()
+    {
+      ++(it_current);
+      return *this;
+    }
 
-            T _begin;
-            T _end; 
+    // i++;
+    // Usually iterators are passed by value and not by const& as they are small.
+    const iterator operator++(int)
+    {
+      iterator tmp = *this;
+     ++it_current;
+      return tmp;
+    }
 
-        public:    
+    bool operator==(const iterator &other) const
+    {
+      return it_current == other.it_current;
+    }
 
-            range(T first, T last) : _begin(first), _end(last){}
-
-            class iterator {
-                
-                private:
-                
-                    T it;
-                
-                public:
-
-                    iterator(T start):  it(start){}
-
-                    iterator* operator++() {
-                        ++it;
-                        return this;
-                    }
-
-                    const iterator operator++(int) {
-                        iterator t = *this;
-                        it++;
-                        return t; 		
-                    }
-
-                    T operator*() const {
-                        return it;
-                    }
-
-                    bool operator==(const iterator& other) const {
-                        return it == (other.it);
-                    }
-
-                    bool operator!=(const iterator& other) const {
-                        return it != (other.it);
-                    }
-            };
-
-            iterator begin() const{
-                return iterator{_begin};
-            }
-            
-            iterator end() const{
-                return iterator{_end};
-            }
-	};
+    bool operator!=(const iterator &other) const
+    {
+      return it_current != other.it_current;
+    }
+    
+  };
+  iterator begin() const { return iterator{_start};}
+  iterator end() const { return iterator{_stop}; }
 };
+}; // namespace itertools
