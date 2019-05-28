f9f719f19fe70c98e9b0291df65d9dff0a29030a
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-28 13:28:38 +0300

    Update powerset.hpp

--- powerset.hpp
+++ powerset.hpp
@@ -1,87 +1,94 @@
-#pragma once
-#include <set> 
-#include <vector>
+#include <bitset>
+#include <algorithm>
 #include <cmath>
-#include <iostream>
-using namespace std;
 
-namespace itertools{
-	
-	template <class T> class powerset{ 
+namespace itertools
+{
+template <typename T>
+class powerset
+{
+private:
+  T set;
+ 
 
-		private:
-		
-            T start;
+public:
+  powerset(T start): set(start) 
+  {
+  }
+  int length() const{
+   return set.length();
+  }
+  class iterator
+  {
+  private:
+     T object;
+     string string_set;
+    int counter = 0;
+    int size ;
+    string subset;
+    
+    void to_string() {
+       stringstream ss;
+       typename T::iterator it  = object.begin();
+      for(int i =0;i<size;i++){
 
-            template <class E> class iterator {
-                public:
-                    E it1;
-                    E it2;
-                    unsigned int I;
-                    unsigned int NumOfE;
-                    
-                    iterator(E itr_start, E itr_fin): it1(itr_start), it2(itr_fin), I(0), NumOfE(0) {
-                        E element_iterator = it1;
-                        while (element_iterator != it2){
-                            ++NumOfE;
-                            ++element_iterator;
-                        }
-                        NumOfE = std::pow(2, NumOfE);
-                    }
-                    
-                    bool operator!=(powerset::iterator<E> const &other) const {
-                        return ((NumOfE - I) != (other.NumOfE - other.I - 1));
-                    }
-                    
-                    auto operator*() const {
-                        E element_iterator = it1;
-                        std::vector<typename std::remove_const<typename std::remove_reference<decltype(*it1)>::type>::type> S;
-                        unsigned int i = I;
-                        while (i != 0 && element_iterator != it2){ 
-                            unsigned int r = i%2;
-                            i = i >> 1; 
-                            if (r == 1)
-                                S.emplace_back(*element_iterator);
-                            ++element_iterator;
-                        }
-                        return S;
-                    }
-                    
-                    powerset::iterator<E> &operator++() {
-                        ++I;
-                        return *this;
-                    }
-            };
-		
-		public:
+      ss << *it;
+      ++it;
+      }
+    ss >> string_set;
+    
+    }
 
-            powerset(T from) : start(from){}
+  public:
+    iterator(T obj):object(obj),size(pow(2,obj.length()))
+    { 
+      to_string();     
+    }
 
-            auto begin() const {
-                return powerset::iterator<decltype(start.begin())> (start.begin(), start.end());
-            } 
-                auto end() const { 
-                return powerset::iterator<decltype(start.begin())> (start.end(), start.end()); 
-            }
-		
-	};
-	
-	template <typename T> powerset<T> _powerset(T from){
-		return powerset<T>(from);
-	}
-	
-	template <typename T> std::ostream &operator<<(std::ostream &stream, const std::vector<T> &S){
-		stream << "{";
-		auto temp = S.begin();
-		if(temp != S.end()){ 
-			stream << *temp; 
-			++temp;
-		}
-		while (temp != S.end()){
-			stream << ',' << *temp;
-			++temp;
-		}
-		stream << "}";
-		return stream;
-	}
+    string operator*() 
+    {
+      if(subset==""){counter++;}
+      return "{"+subset+"}"; 
+    }
+    
+
+
+    // ++i;
+    iterator operator++()
+    {
+      stringstream ss;
+      
+      string binary = bitset<20>(counter).to_string(); //to binary
+      reverse(binary.begin(),binary.end());
+      for (int i=0;i<size;i++){
+       
+        if(binary[i]=='1'){
+          ss << string_set[i];
+          ss <<",";
+         
+        }
+      }
+      ss>>subset;
+      
+     subset=subset.substr(0,subset.length()-1);      
+      ++counter;
+      return *this;
+    }
+
+    bool operator==(const iterator other) 
+    {
+      return *this == other;
+    }
+
+    bool operator!=(const iterator other)
+    {
+      
+
+      return counter <=size;
+    }
+    
+  };
+  iterator begin() const { return iterator{set};}
+  iterator end() const { return iterator{set}; }
+};
 };
